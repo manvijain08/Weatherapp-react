4c8d9a1a01e2570c9525b6d80679f56277a3b624
manvijain08 <jainmanvi017@gmail.com>
2020-08-19 22:11:57 +0530

    Updates

---
+++ precache-manifest.89a6d093fbc9224913935dbc1ea01b2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7382258b603ef8fc3463fb4702d4fad0",
+    "url": "/Weatherapp-react/index.html"
+  },
+  {
+    "revision": "077468c764460aea8773",
+    "url": "/Weatherapp-react/static/css/main.b73f0f3e.chunk.css"
+  },
+  {
+    "revision": "8e5f7d5eb15416ee0bab",
+    "url": "/Weatherapp-react/static/js/2.481c64a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weatherapp-react/static/js/2.481c64a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "077468c764460aea8773",
+    "url": "/Weatherapp-react/static/js/main.62d702a9.chunk.js"
+  },
+  {
+    "revision": "a18b3db31caccf233bcf",
+    "url": "/Weatherapp-react/static/js/runtime-main.7359291d.js"
+  }
+]);
